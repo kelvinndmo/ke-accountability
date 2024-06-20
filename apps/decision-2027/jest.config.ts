83c42e267ea9a1aed9a9c5fe4610045f44469d90
Kelvin Onkundi <kelvin@ndemo.io>
2024-06-20 18:00:26 +0300

    Initial commit

---
+++ apps/decision-2027/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'decision-2027',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/decision-2027',
+};
